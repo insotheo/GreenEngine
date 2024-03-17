0b61038458f03c97f0adca9a612983d2381d95a3
insotheo <maxim.insotheo@gmail.com>
2024-03-17 11:53:24 +0400

    project setup

--- premake5.lua
+++ premake5.lua
@@ -14,4 +14,61 @@ project "GreenEngine"
      language "C++"
      targetdir("bin/"..outputdirectory.."/%{prj.name}")
      objdir("bin-inter/"..outputdirectory.."/%{prj.name}")
-     
+
+     files{
+          "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"
+     }
+
+     includedirs{
+          "%{prj.name}/src"
+     }
+
+     filter "system:windows"
+          cppdialect "C++17"
+          staticruntime "On"
+          systemversion "latest"
+          defines{
+               "GE_PLATFORM_WINDOWS", "GE_BUILD_DLL"
+          }
+          postbuildcommands{
+               ("{COPY} %{cfg.buildtarget.relpath} ../bin/"..outputdirectory.."/DevDemoGame")
+          }
+          filter "configurations:Debug"
+               defines "GE_CONFIGURATION_DEBUG"
+               symbols "On"
+          filter "configurations:Release"
+               defines "GE_CONFIGURATION_RELEASE"
+               optimize "On"
+
+project "DemoGame"
+     location "DemoGame"
+     kind "ConsoleApp"
+     language "C++"
+     targetdir("bin/"..outputdirectory.."/%{prj.name}")
+     objdir("bin-inter/"..outputdirectory.."/%{prj.name}")
+
+     files{
+          "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"
+     }
+
+     includedirs{
+          "GreenEngine/src"
+     }
+
+     links{
+          "GreenEngine"
+     }
+
+     filter "system:windows"
+          cppdialect "C++17"
+          staticruntime "On"
+          systemversion "latest"
+          defines{
+               "GE_PLATFORM_WINDOWS"
+          }
+          filter "configurations:Debug"
+               defines "GE_CONFIGURATION_DEBUG"
+               symbols "On"
+          filter "configurations:Release"
+               defines "GE_CONFIGURATION_RELEASE"
+               optimize "On"
