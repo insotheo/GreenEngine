d2b4680599f7c80318a88cb479c38f7da79660b7
insotheo <maxim.insotheo@gmail.com>
2024-03-20 19:18:46 +0400

    C# setup

---
+++ DemoGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using GreenEngine;
+
+namespace DemoGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Hello();
+        }
+    }
+}
---
+++ GreenEngine/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GreenEngine
+{
+    public class Class1
+    {
+
+        public static void Hello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
