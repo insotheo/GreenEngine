a7e0b415956b8c318930648fc4cabaff7e917802
insotheo <maxim.insotheo@gmail.com>
2024-03-24 19:30:33 +0400

    Collision

--- DemoGame/GameAssets/Scenes/TestScene.cs
+++ DemoGame/GameAssets/Scenes/TestScene.cs
@@ -9,6 +9,9 @@ namespace DemoGame.GameAssets.Scenes
     public class TestScene : Scene2D
     {
         public GameObject go;
+        public GameObject go2;
+        float speed = 2;
+
 
         public TestScene() : base(new ColorClass("#FFFFFF"),
             new Camera2D(new Vector2D(), Vector2D.OneVector2D(), 0), 
@@ -18,19 +21,32 @@ namespace DemoGame.GameAssets.Scenes
         public override void OnLoad()
         {
             go = new GameObject(new Vector2D(100, 500), new Vector2D(50, 50), "GameAssets\\InsotheoLogo.png", "a");
+            go2 = new GameObject(new Vector2D(400, 400), new Vector2D(50, 50), "GameAssets\\InsotheoLogo.png", "a");
+            go2.HasCollision = true;
+            go.HasCollision = false;
             go.BodyType = PhysicsConstants.BodyType2D.Rigid;
-            go.Mass = 1f;
+            go.Mass = 1.25f;
             
-        }
-
-        public override void OnUpdate()
-        {
-            Log.Warning(go.Position.Y);
         }
 
         public override void OnKeyDown(KeyEventArg e)
         {
-            go.AddForce(new Vector2D(3, 2), deltaTime, 2);
+            if(e.Key == Key.KeyCode.W)
+            {
+                go.AddForce(new Vector2D(0, -speed), deltaTime, 2);
+            }
+            else if(e.Key == Key.KeyCode.S)
+            {
+                go.AddForce(new Vector2D(0, speed), deltaTime, 2);
+            }
+            else if(e.Key == Key.KeyCode.D)
+            {
+                go.AddForce(new Vector2D(speed, 0), deltaTime, 2);
+            }
+            else if (e.Key == Key.KeyCode.A)
+            {
+                go.AddForce(new Vector2D(-speed, 0), deltaTime, 2);
+            }
         }
     }
 }
--- GreenEngineAPI/Physics/GameObject.cs
+++ GreenEngineAPI/Physics/GameObject.cs
@@ -1,4 +1,7 @@
 ﻿using GreenEngineAPI.Graphics;
+using System;
+using GreenEngineAPI.Core;
+using System.Security.Policy;
 
 namespace GreenEngineAPI.Physics
 {
@@ -51,9 +54,29 @@ namespace GreenEngineAPI.Physics
 
         public void SimulatePhysics(float deltaTime)
         {
+            if (RendererGameWindow.SceneManager.GetCurrentScene().PhysicsObjects.Count - 1 > 0)
+            {
+                foreach (GameObject obj in RendererGameWindow.SceneManager.GetCurrentScene().PhysicsObjects)
+                {
+                    if (obj != this)
+                    {
+                        if (IsCollided(obj))
+                        {
+                            Log.Info("On collided");
+                        }
+                    }
+                }
+            }
             if(BodyType != PhysicsConstants.BodyType2D.Static)
             {
-                Acceleration.Y -= 1;
+                if (Acceleration.Y > 0)
+                {
+                    Acceleration.Y += deltaTime;
+                }
+                else
+                {
+                    Acceleration.Y -= deltaTime;
+                }
                 FallForce = Mass * PhysicsConstants.g; //F = mg
                 FrictionForce = Acceleration.Y != 0 && Acceleration.X != 0 ? Friction * Mass * PhysicsConstants.g * -1 : 0;
 
@@ -72,14 +95,35 @@ namespace GreenEngineAPI.Physics
         {
             GettingForce = ((Mass * deltaVelocity) / deltaTime);
             Acceleration.X += GettingForce * direction.X;
+            if(Acceleration.X != 0)
+            {
+                Acceleration.X += direction.X;
+            }
             Acceleration.Y += GettingForce * -direction.Y;
         }
 
         public bool IsCollided(GameObject body)
         {
-            if (!body.HasCollision || !HasCollision) {  return false; }
-            
-            return false;
+            if (!body.HasCollision || !HasCollision) { return false; }
+            else
+            {
+                if (this.Position.X + ColliderRadius <= body.Position.X + body.Scale.X &&
+                        this.Position.X + this.Scale.X - ColliderRadius >= body.Position.X &&
+                        this.Position.Y + ColliderRadius <= body.Position.Y + body.Scale.Y &&
+                        this.Position.Y + this.Scale.Y - ColliderRadius >= body.Position.Y)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void InteractCollidedObject(GameObject obj, Vector2D interactionVector)
+        {
+
         }
     }
 }
